c195836bdf6f1a9e66d11d1154fe6823deb00c04
soumy <soumy@DESKTOP-0HEPKBP.home>
2019-03-18 18:03:50 -0700

    commit 1

---
+++ org.test.com/src/main/java/org/test/com/org/test/com/App.java
@@ -0,0 +1,15 @@
+package org.test.com.org.test.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+     
+    }
+}
